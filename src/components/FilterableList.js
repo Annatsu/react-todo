b1b2fadde9a5e9c4aa291fb2384e79133e3a033d
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 02:11:12 -0300

    WIP - FilterButtons HOC

--- src/components/FilterableList.js
+++ src/components/FilterableList.js
@@ -10,7 +10,44 @@ import FilterButton from "./FilterButton";
 // Application's constants
 import Constants from "../constants";
 
+
+// HOC for the FilterButton components.
+const FilterButtons = ({ changeFilter, children }) => {
+  const _children = React.cloneElement(children, { changeFilter });
+
+  console.log('children', children)
+  console.log('_children', _children)
+
+  return (
+    <div>
+      {_children}
+    </div>
+  );
+}
+
+
 const FilterableList = ({ filter, changeFilter, children }) => {
+  const _children = React.cloneElement(children, { filter });
+
+  console.log('filterable children', children);
+  console.log('filterable _children', _children);
+
+  return (
+    <Fragment>
+
+      <div>
+        {_children}
+      </div>
+
+      <FilterButtons changeFilter={changeFilter}>
+        <FilterButton filter={Constants.filters.FILTER_SHOW_ALL}>Show All</FilterButton>
+        <FilterButton filter={Constants.filters.FILTER_SHOW_COMPLETED}>Show Completed</FilterButton>
+        <FilterButton filter={Constants.filters.FILTER_SHOW_UNCOMPLETED}>Show Uncompleted</FilterButton>
+      </FilterButtons>
+
+    </Fragment>
+  );
+};
 
 
 FilterableList.propTypes = {
