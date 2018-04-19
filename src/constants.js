d590b8b630c062f7dd6c6231c4501cbee8c42188
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 02:10:42 -0300

    Simplified the constants behavior

--- src/constants.js
+++ src/constants.js
@@ -1,41 +1,36 @@
 /**
  * Shows all the todos in the App's state.
  *
- * @export
  * @constant
  */
-export const FILTER_SHOW_ALL = 0;
+const FILTER_SHOW_ALL = 0;
 
 
 /**
  * Only display the completed todos.
  *
- * @export
  * @constant
  */
-export const FILTER_SHOW_COMPLETED = 1;
+const FILTER_SHOW_COMPLETED = 1;
 
 
 /**
  * Only display the uncompleted todos.
  *
- * @export
  * @constant
  */
-export const FILTER_SHOW_UNCOMPLETED = 1;
+const FILTER_SHOW_UNCOMPLETED = 1;
 
 
 /**
  * The default export is an object containing all of the constants definition.
- * 
+ *
  * @export
  */
-const defaultExport = {
+export default {
   filters: {
     FILTER_SHOW_ALL,
     FILTER_SHOW_COMPLETED,
     FILTER_SHOW_UNCOMPLETED
   }
 };
-
-export default defaultExport;
