89dcc762262df59c1a7f8de4c7f20ed178775282
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 00:46:35 -0300

    Created a static todo list just to style the component

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,44 @@ import Constants from '../constants';
 class App extends Component {
 
     state = {
-        todos: [],
+        todos: [
+            {
+                desc: 'Study React'
+            },
+            {
+                desc: 'Study TDD'
+            },
+            {
+                desc: 'Be nice to people :)'
+            }
+        ],
         filter: Constants.filters.FILTER_SHOW_ALL
     }
 
 
     render() {
 
+
+    render() {
+        const {
+            todos,
+            filter
+        } = this.state;
+
+
+        return (
+            <div>
+                <ul>
+                    {
+                        todos.map((todo, i) => (
+                            <li key={i}>
+                                {todo.desc}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        );
     }
 
 }
