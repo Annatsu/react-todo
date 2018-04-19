142c5894f869808beb5c4d023f7a5c700a19d93f
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 00:38:24 -0300

    App component structure

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+// React
+import React, { Component } from 'react';
+
+const Constants = require('../constants');
+
+
+class App extends Component {
+
+    state = {
+        todos: [],
+        filter: Constants.filters.FILTER_SHOW_ALL
+    }
+
+
+    render() {
+
+    }
+
+}
+
+
+export default App;
