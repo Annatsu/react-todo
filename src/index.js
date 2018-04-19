c9ed76bfaba5d99d10588f4627107c62b90c70af
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 00:45:57 -0300

    Mounting the App into the DOM

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+import App from './components/App';
+
+
 ReactDOM.render(
-    <div/>,
+    <App/>,
     document.getElementById('root')
 );
