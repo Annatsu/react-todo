5c1855bc08a956432316878e0bde85b922f4de3a
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 01:47:28 -0300

    Added the FilterButton

---
+++ src/components/FilterButton.js
@@ -0,0 +1,19 @@
+// React
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const FilterButton = ({ changeFilter, filter, children }) => (
+  <button onClick={() => changeFilter(filter)}>
+    {children}
+  </button>
+);
+
+
+FilterButton.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+  filter: PropTypes.number.isRequired
+};
+
+
+export default FilterButton;
