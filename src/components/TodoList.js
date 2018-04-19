57daf9ba52c91924237e799ce9c06623e32088ae
Brendon Pagano <brendonpagano@gmail.com>
2018-04-19 02:24:54 -0300

    Added TodoList filtering functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,17 +3,33 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-const TodoList = ({ todos, filter }) => (
-  <ul>
-    {
-      todos.map((todo, i) => (
-        <li key={i}>
-          {todo.desc}
-        </li>
-      ))
-    }
-  </ul>
-);
+// Application's Constants
+import Constants from '../constants';
+
+
+const completeCompare = {
+  [Constants.filters.FILTER_SHOW_COMPLETED]: true,
+  [Constants.filters.FILTER_SHOW_UNCOMPLETED]: false
+};
+
+
+const TodoList = ({ todos, filter }) => {
+  const shownTodos = filter === Constants.filters.FILTER_SHOW_ALL
+    ? todos
+    : todos.filter((todo) => todo.completed === completeCompare[filter]);
+
+  return (
+    <ul>
+      {
+        shownTodos.map((todo, i) => (
+          <li key={i}>
+            {todo.desc}
+          </li>
+        ))
+      }
+    </ul>
+  );
+};
 
 
 TodoList.propTypes = {
